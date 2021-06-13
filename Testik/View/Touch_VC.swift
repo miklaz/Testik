5471dde21f30af8f595e586fd46f56f6c3b62f70
Mikhail <mikl.a.z1998@gmail.com>
2021-06-13 14:30:36 +0300

    Add Touch, RGB and B&W+Gray tests

---
+++ Testik/View/Touch_VC.swift
@@ -0,0 +1,101 @@
+//
+//  Touch_VC.swift
+//  Testik
+//
+//  Created by Михаил Зайцев on 12.06.2021.
+//
+
+
+import UIKit
+
+class Canvas: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        // custom dawing
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        
+        /*
+        let startPoint = CGPoint(x: 0, y: 0)
+        let endPoint = CGPoint(x: 100, y: 100)
+        context.move(to: startPoint)
+        context.addLine(to: endPoint)
+        */
+        
+        context.setStrokeColor(UIColor.white.cgColor)
+        context.setLineWidth(60)
+        context.setLineCap(.round)
+        
+        lines.forEach { (line) in
+            for (i, p) in line.enumerated() {
+                if i == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+
+        
+        context.strokePath()
+    }
+    
+    //var line = [CGPoint]()
+    var lines = [[CGPoint]]()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else {
+            return}
+        
+        //print (point)
+        
+        guard var lastLine = lines.popLast() else {
+            return
+        }
+        lastLine.append(point)
+        lines.append(lastLine)
+        
+        //var lastLine = lines.last
+        //lastLine?.append(point)
+        //line.append(point)
+        
+        setNeedsDisplay()
+    }
+    
+}
+
+class Touch_VC: UIViewController {
+
+    var canvas = Canvas()
+    
+    @IBOutlet var dismissButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.addSubview(canvas)
+        canvas.backgroundColor = .orange
+        canvas.frame = view.frame
+        view.addSubview(dismissButton)
+        
+        if canvas.backgroundColor == .green {
+            print ("lol")
+        }
+        
+    }
+    
+    @IBAction func dismissAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
